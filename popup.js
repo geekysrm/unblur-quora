3c60f2e9d87bbafc2e808a316c920f3069b7b381
geekysrm <soumyacool2012@gmail.com>
2020-01-04 02:42:41 +0530

    add CSS and other logic

--- popup.js
+++ popup.js
@@ -2,12 +2,11 @@ document.addEventListener(
   "DOMContentLoaded",
   function() {
     var unblurButton = document.getElementById("unblur");
-    console.log("hey");
+
     chrome.tabs.query(
       { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
       function(tabs) {
-        // alert(tabs[0].url);
-        const currentURL = tabs[0].url;
+        let currentURL = tabs[0].url;
         console.log("Current URL: " + currentURL);
         if (currentURL.indexOf("https://www.quora.com") > -1) {
           unblurButton.addEventListener(
@@ -15,9 +14,12 @@ document.addEventListener(
             function() {
               console.log("unblur button clicked");
               unblurButton.disabled = true;
-              // console.log(currentURL);
-              if (!currentURL.endsWith("?share=1"))
+              if (!currentURL.endsWith("?share=1")) {
+                // Removing last # if present:
+                if (currentURL.endsWith("#"))
+                  currentURL = currentURL.slice(0, -1);
                 chrome.tabs.update(tabs.id, { url: currentURL + "?share=1" });
+              } else unblurButton.innerHTML = "Unblurred";
             },
             false
           );
@@ -27,3 +29,5 @@ document.addEventListener(
   },
   false
 );
+
+// TODO: Make it pretty
